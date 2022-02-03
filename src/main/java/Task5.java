6e9bdf7fc7d17ec107ced9fc8ed9644d405df6bf
border <border@student.21-school.ru>
2022-02-03 18:37:24 +0300

    5 задача не решена еще

--- src/main/java/Task5.java
+++ src/main/java/Task5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Task5 {
     public static void main(String[] args) {
@@ -6,13 +7,24 @@ public class Task5 {
         int n = scanner.nextInt();
         int[] jumpLengths = new int[n];
         int[] slidingLengths = new int[n];
+        int[] jumpsNumber = new int[n + 1];
+        Arrays.fill(jumpsNumber, -1);
 
-        for (int i = 0; i < jumpLengths.length; i++) {
+        for (int i = jumpLengths.length - 1; i >= 0; --i) {
             jumpLengths[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < slidingLengths.length; i++) {
+        for (int i = slidingLengths.length - 1; i >= 0; --i) {
             slidingLengths[i] = scanner.nextInt();
         }
+
+        System.out.println(Arrays.toString(jumpLengths));
+        System.out.println(Arrays.toString(slidingLengths));
+
+        int step = 0;
+        while (jumpsNumber[n] == -1) {
+        }
+
+        System.out.println(Arrays.toString(jumpsNumber));
     }
 }
