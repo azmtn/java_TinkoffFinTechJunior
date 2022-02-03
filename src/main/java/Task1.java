04182841e1b3bd5a496d075c7e266aea9fb248cc
border <border@student.21-school.ru>
2022-02-03 20:36:54 +0300

    task 1

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        scanner.close();
+        if (a > b && (a + b) % 2 == 0 && (a - b) >= 2 * n) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
