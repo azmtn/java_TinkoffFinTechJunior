f04ae649acb37ae3091bffa934e5e8a57e8fdf28
border <border@student.21-school.ru>
2022-02-03 20:44:15 +0300

    task 2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+        scanner.close();
+
+        int count = 0;
+        while (n > 0 && m > 0) {
+            if (n > m) {
+                n -= m;
+            } else {
+                m -= n;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
 }
