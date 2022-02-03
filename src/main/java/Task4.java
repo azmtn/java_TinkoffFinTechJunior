f4154c0300a793f5263e1d5816b96cc6a38ec450
border <border@student.21-school.ru>
2022-02-03 21:52:45 +0300

    task 4

--- src/main/java/Task4.java
+++ src/main/java/Task4.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        scanner.close();
 
+        long[][] chessField = new long[n+1][m+1];
+        chessField[1][1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= m; j++) {
+                chessField[i][j] = chessField[i - 1][j - 2] + chessField[i - 2][j - 1];
+            }
+        }
+        System.out.println(chessField[n][m]);
     }
 }
